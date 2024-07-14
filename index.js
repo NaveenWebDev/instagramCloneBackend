0336ee745ddd3eebdbe7a9884fc148780237f213
Naveen Sharma <ns222280@gmail.com>
2024-07-14 11:36:50 +0530

    socket added and real time chat app is ready

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const cors = require("cors");
 const router = require("./routes/router");
 const fileupload = require("express-fileupload");
 cloudinary.cloudinaryConnect();
+const {Server} = require("socket.io");
+const http = require("http");
+
+const server = http.createServer(app);
+const io = new Server (server, {
+    cors:{
+        origin:"*"
+    },
+    credentials:true,
+})
 
 //middleware
 app.use(express.json());
@@ -17,10 +27,24 @@ app.use(fileupload({
 })) 
 app.use('/api/v1', router)
 
+io.on("connection", (socket)=>{
+    console.log("userConnted id is =", socket.id);
+
+    //message for chat
+    socket.on("message", (data)=>{
+        console.log(data);
+        socket.broadcast.emit("message", data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+})
+
 
 const port = process.env.PORT || 8000
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`app is running on ${port} number`);
 })
 
